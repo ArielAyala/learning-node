2e7912a8c057de6277ee9f10c082e4bb4a7fefc9
ArielAyala <arielayalamail@gmail.com>
2021-09-26 14:55:40 -0400

    CRUD example

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -17,6 +17,58 @@ const moviesApi = (app) => {
       next(error);
     }
   });
+
+  router.get('/:movieId', async (req, res, next) => {
+    try {
+      const movies = await Promise.resolve(moviesMock[0]);
+
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrieved',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async (req, res, next) => {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'movie created',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:moviedId', async (req, res, next) => {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movie updated',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:moviedId', async (req, res, next) => {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'movie deleted',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 };
 
 module.exports = moviesApi;
