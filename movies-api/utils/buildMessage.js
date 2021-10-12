94300ff3dea30c82304bda3d422d1cf9d36f10e1
ArielAyala <arielayalamail@gmail.com>
2021-10-12 19:27:19 -0300

    Test for utilies

---
+++ movies-api/utils/buildMessage.js
@@ -0,0 +1,10 @@
+const buildMessage = (entity, action) => {
+
+    if (action === 'list') {
+        return `${entity}s ${action}ed`;
+    }
+
+    return `${entity} ${action}d`;
+}
+
+module.exports = buildMessage;
