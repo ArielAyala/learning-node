196ca9e526ee4fe27b470ee0c5589629c9e49e8a
ArielAyala <arielayalamail@gmail.com>
2021-10-12 20:10:31 -0300

    Debugging

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express('./config/index');
 const { config } = require('./config');
+const debug = require('debug')('app:server');
 
 const moviesApi = require('./routes/movies');
 
@@ -22,5 +23,6 @@ app.use(errorHandler);
 
 
 app.listen(config, () => {
-  console.log(`Listening http://localhost:${config.port}`);
+  debug(`Listening http://localhost:${config.port}`)
+  // console.log(`Listening http://localhost:${config.port}`);
 });
