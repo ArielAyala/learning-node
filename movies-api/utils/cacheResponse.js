4e0b0cb948818fe0606471463f09d63a2cb146e5
ArielAyala <arielayalamail@gmail.com>
2021-10-16 13:01:12 -0300

    Delete log

--- movies-api/utils/cacheResponse.js
+++ movies-api/utils/cacheResponse.js
@@ -1,7 +1,6 @@
 const { config } = require('../config');
 
 function cacheResponse(res, seconds) {
-    console.log('seconds', seconds);
     if (!config.dev) {
         res.set('Cache-Control', `public, max-age=${seconds}`)
     }
